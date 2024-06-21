d878bfe35982e18623e169fe86261171dd180904
Kleber Posligua Flores <kleberposligua@hotmail.com>
2024-06-21 07:19:19 -0500

    OBSERVACIONES Ecuacion_cuadratica.cpp

--- Ecuacion_cuadratica.cpp
+++ Ecuacion_cuadratica.cpp
@@ -8,6 +8,8 @@ using namespace std;
 
 bool EcuacionCuadratica(double a, double b, double c, double& x1, double& x2) 
 {
+ //FALTA VERIFICAR SI "A" ES CERO
+    
     double discriminante = b * b - 4 * a * c;
     if (discriminante < 0) 
     {
